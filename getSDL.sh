3ceb237590d96ec201ad9b79ca6e32849cab4f20
VDavid003 <virag.david003@gmail.com>
2020-08-08 03:13:34 +0200

    Actually we'll need to symlink that

--- getSDL.sh
+++ getSDL.sh
@@ -5,8 +5,8 @@ pushd app/jni/SDL
 wget https://www.libsdl.org/release/SDL2-2.0.12.zip
 unzip -q SDL2-2.0.12.zip
 mv SDL2-2.0.12/src src
-mkdir include
-mv SDL2-2.0.12/include include/SDL2
+mv SDL2-2.0.12/include include
+ln -s . include/SDL2
 rm SDL2-2.0.12.zip
 rm -r SDL2-2.0.12
 
