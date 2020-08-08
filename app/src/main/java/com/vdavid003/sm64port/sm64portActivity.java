febd9686c40364abf1430291d0d627f2a0a5c6aa
VDavid003 <virag.david003@gmail.com>
2020-08-08 02:58:46 +0200

    Initial Commit

---
+++ app/src/main/java/com/vdavid003/sm64port/sm64portActivity.java
@@ -0,0 +1,11 @@
+package com.vdavid003.sm64port;
+
+import org.libsdl.app.SDLActivity;
+
+public class sm64portActivity extends SDLActivity
+{
+    @Override
+    protected String getMainFunction() {
+        return "main";
+    }
+}
---
+++ getSDL.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+pushd app/jni/SDL
+
+wget https://www.libsdl.org/release/SDL2-2.0.12.zip
+unzip -q SDL2-2.0.12.zip
+mv SDL2-2.0.12/src src
+mkdir include
+mv SDL2-2.0.12/include include/SDL2
+rm SDL2-2.0.12.zip
+rm -r SDL2.2.0.12
+
+popd
