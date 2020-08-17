ba17b9fbd2b59a3800cad63b1fd5810922a5e283
VDavid003 <virag.david003@gmail.com>
2020-08-18 01:02:55 +0200

    Add Windows building support, and add instructions

--- getSDL.sh
+++ getSDL.sh
@@ -6,7 +6,14 @@ wget https://www.libsdl.org/release/SDL2-2.0.12.zip
 unzip -q SDL2-2.0.12.zip
 mv SDL2-2.0.12/src src
 mv SDL2-2.0.12/include include
-ln -s . include/SDL2
+
+#MSYS2's symlinks seem to just copy the folder which isn't really a good thing recursively. Native symlinks are broken for me so let's just use Windows's implementation
+if [[ -n "$WINDIR" ]]; then
+    cmd <<< "mklink /D include\SDL2 ."
+else
+    ln -s . include/SDL2
+fi
+
 rm SDL2-2.0.12.zip
 rm -r SDL2-2.0.12
 
